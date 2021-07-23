cdaac621990a52ec53983fa7f2cac05071e960d1
Owen Harrington-Woodard <62514173+owenhw@users.noreply.github.com>
2021-07-23 13:16:40 -0400

    Create get_stations.py
    get data for the indego_stations table

---
+++ get_stations.py
@@ -0,0 +1,23 @@
+# imports
+import civis
+import from indego import Indego
+import pandas as pd
+
+# initialize an indego object
+ind = Indego()
+
+#get the stations and put them in a dataframe
+data = ind.get_stations()
+
+df=pd.DataFrame(data).T
+
+df = df.loc[:, ['id', 'name', 'totalDocks', 'kioskType', 'addressStreet', 'addressCity',
+       'addressState', 'addressZipCode', 'closeTime', 'eventEnd',
+       'eventStart', 'isEventBased', 'isVirtual', 'timeZone', 'latitude',
+       'longitude']]
+       
+
+# import the station data into civis
+io.dataframe_to_civis(df2, database = 'redshift-general'
+                      , table='oharringtonwoodard.indego_stations'
+                      , existing_table_rows='drop')
